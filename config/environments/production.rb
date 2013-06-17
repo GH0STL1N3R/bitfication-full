8d51c90d97dbe457e8314c6f600fe6e4cd88b35a
William Moss <willmoss26@gmail.com>
2013-06-17 15:10:12 -0700

    fixed untracked files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,23 +5,34 @@ BitcoinBank::Application.configure do
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.force_ssl = true
+  config.force_ssl = false
   
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = {
-    :host => "bitfication.com"
+  
+  # configure mailer
+  ENV["MANDRILL_USERNAME"] = "willmoss26@gmail.com"
+  ENV["MANDRILL_PASSWORD"] = "Mnkx71AphDFJeCbWKhr75w"
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :port      => 587,
+    :address   => "smtp.mandrillapp.com",
+    :user_name => "willmoss26@gmail.com",
+    :password  => "Mnkx71AphDFJeCbWKhr75w"
   }
   
-  config.middleware.use ::ExceptionNotifier,
-    :email_prefix => "[BF Exception] ",	 	
-    :sender_address => %w{no-reply@bitfication.com},	 
-    :exception_recipients => %w{support@bitfication.com}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
   # Used to broadcast invoices public URLs
+  
+  # BITFICATION DEFAULTS
   config.base_url = "https://bitfication.com/"
   config.assets.initialize_on_precompile = true
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
   config.serve_static_assets = false
+  
+  
+  
 end
