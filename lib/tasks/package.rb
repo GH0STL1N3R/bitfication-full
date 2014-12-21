121d4f0f0ddc9d61bc2f7d1295442982e20155ca
Thiago Martins <thiago@bitfication.com>
2014-12-21 01:03:53 -0200

    Adding postinstall script plus new version of package.rb.

--- lib/tasks/package.rb
+++ lib/tasks/package.rb
@@ -1,5 +1,7 @@
 class MyFPM
-  attr_accessor :name, :version, :dependencies, :files, :exclude, :destination, :description, :vendor, :maintainer
+
+  attr_accessor :name, :version, :dependencies, :files, :exclude
+  attr_accessor :destination, :description, :vendor, :maintainer, :post_install
 
   def initialize(name, version)
     @name = name
@@ -26,11 +28,12 @@ class MyFPM
 
   def fpm_cmd
     cmd = "fpm -s dir -t deb -n #{@name} -v #{@version} -a all --prefix #{@destination}"
-    cmd += " --vendor #{@vendor}" if @vendor.present?
-    cmd += " --description '#{@description}'" if @description.present?
-    cmd += " -m #{@maintainer}" if @maintainer.present?
+    cmd += " --vendor #{@vendor}" unless @vendor.nil?
+    cmd += " --description '#{@description}'" unless @description.nil?
+    cmd += " -m #{@maintainer}" unless @maintainer.nil?
     cmd += " -d '#{@dependencies.join('\',\'')}' " if @dependencies.any?
     cmd += " --exclude #{@exclude.join(' --exclude ')}" if @exclude.any?
+    cmd += " --after-install #{@post_install}" unless @post_install.nil?
     cmd += " #{pack_files}"
 
     puts "Generating package:\n#{cmd}"
@@ -40,22 +43,28 @@ class MyFPM
 end
 
 task :package, [:version, :includes, :excludes] do |t, args|
-  if args.empty?
-    abort "Needs to pass almost a version to package.\nUsage: rake package['version', 'opt. files to include', opt. files to exclude']"
+
+  if args.version.nil?
+    abort "Need to pass at least the version to package.\nUsage: rake package['version', 'opt. files to include', opt. files to exclude']"
   end
 
   fpm = MyFPM.new('bitfication', args.version)
   fpm.destination = '/opt/bitfication'
 
-  opt_files = args.includes.present? ? args.includes.split(' ') : [ ]
-  opt_excludes = args.excludes.present? ? args.excludes.split(' ') : [ ]
+  opt_files = args.includes.nil? ? [] : args.includes.split(' ')
+  opt_excludes = args.excludes.nil? ? [] : args.excludes.split(' ')
 
   fpm.files = Dir['*'] + opt_files
-  fpm.exclude = ['test', 'spec', 'tmp', 'log'] + opt_excludes
-  fpm.dependencies = [ 'apache2', 'ruby', 'rails' ]
+  fpm.exclude = ['spec'] + opt_excludes
+
+  fpm.dependencies = [ 'ruby', 'rails', 'bundler', 'unicorn', 'ruby-jquery-rails', 'ruby-mysql2', 'ruby-will-paginate' ]
+
+  fpm.post_install = './misc/postinstall.sh'
+
   fpm.vendor = 'Bitfication'
-  fpm.maintainer = 'thiagocmartinsc@gmail.com'
-  fpm.description = 'Bitfication CryptoCurrency Open Source Exchange'
+  fpm.maintainer = 'thiago@bitfication.com'
+  fpm.description = 'Bitfication, a Open Source CryptoCurrency Exchange'
 
   fpm.package!
+
 end
---
+++ misc/postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/bitfication
+bundle install
