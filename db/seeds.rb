0c0071a6340a5121072a68cb83d439afe499edc0
Thiago Martins <thiago@bitfication.com>
2015-05-03 02:56:06 -0300

    Improving db/seeds.rb, currencies are now created by rake db:setup. About Issue 4.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Admin.create! do |user|
   user.password = "2QmaKCs2fhU8x9iS0AAL4F7ACR3aZxNdq6WyTEE1"
   user.password_confirmation = "2QmaKCs2fhU8x9iS0AAL4F7ACR3aZxNdq6WyTEE1"
-  user.email = "admin@localhost.local"
+  user.email = "admin@testnet.bitfication.com"
   user.skip_captcha = true
   user.confirmed_at = DateTime.now
 end
 
-puts "Created \"admin@localhost.local\" user with password \"2QmaKCs2fhU8x9iS0AAL4F7ACR3aZxNdq6WyTEE1\""
+puts "Created \"admin@testnet.bitfication.com\" user with password \"2QmaKCs2fhU8x9iS0AAL4F7ACR3aZxNdq6WyTEE1\""
 
 Stats.create! do |s|
   s.volume = 0
@@ -14,3 +14,29 @@ Stats.create! do |s|
   s.plow = 0
   s.pwavg = 0
 end
+
+puts "Created zeroed stats."
+
+Currencies.create! do |btc|
+  btc.code = "BTC"
+  btc.created_at = DateTime.now
+  btc.updated_at = DateTime.now
+  btc.display = "BTC$"
+  btc.flag = ""
+  btc.pt = "Bitcoins"
+  btc.en = "Bitcoins"
+end
+
+puts "Created BTC currency."
+
+Currencies.create! do |brl|
+  brl.code = "BRL"
+  brl.created_at = DateTime.now
+  brl.updated_at = DateTime.now
+  brl.display = "R$"
+  brl.flag = "pt"
+  brl.pt = "Reais"
+  brl.en = "Brazilian Real"
+end
+
+puts "Created BRL currency."
